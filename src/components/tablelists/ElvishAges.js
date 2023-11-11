838af747c7c92b8573b1fcf3869eab47082fd394
MatthewGermon <mjgermon@gmail.com>
2023-11-11 15:35:54 +1100

    Update Elvish Table

--- src/components/tablelists/ElvishAges.js
+++ src/components/tablelists/ElvishAges.js
@@ -20,7 +20,7 @@ function CalcMEFourthAgeOne(index) {
     if (index <= 20) {
         return index * 10;
     } else {
-        return (index - 20) * 72 + 120;
+        return (index - 20) * 72 + 200;
     }
 }
 
@@ -28,7 +28,7 @@ function CalcMEFourthAgeTwo(index) {
     if (index <= 20) {
         return index * 10;
     } else {
-        return (index - 20) * 48 + 60;
+        return (index - 20) * 48 + 200;
     }
 }
 
@@ -49,26 +49,26 @@ function CalcReck(index, olmen) {
         return coimen + " LY";
     }
     if (coimen === 73) {
-        return "Vinimetta";
+        return "Old Age";
     }
     return "";
 }
 
-// function CalcYeniLoar(age) {
-//     var result = "";
-//     var yen = Math.floor(age / 144);
-//     if (yen > 0) {
-//         result += yen + " yén"
-//     }
-//     var loa = age % 144;
-//     if (loa > 0 && yen > 0) {
-//         result += ", " + loa + " löa"
-//     } else if (loa > 0) {
-//         result += loa + " löa"
-//     }
+function CalcYeniLoar(age) {
+    var result = "";
+    var yen = Math.floor(age / 144);
+    if (yen > 0) {
+        result += yen + "Y"
+    }
+    var loa = age % 144;
+    if (loa > 0 && yen > 0) {
+        result += ", " + loa + "L"
+    } else if (loa > 0) {
+        result += loa + "L"
+    }
 
-//     return result;
-// }
+    return result;
+}
 
 function Rows() {
     var rows = [];
@@ -79,19 +79,20 @@ function Rows() {
             <tr>
                 <td>{index}</td>
                 <td>{CalcReck(index, 24)}</td>
-                {/* <td>{CalcYeniLoar(CalcAge(index, 24))}</td> */}
+                <td>{CalcYeniLoar(CalcAge(index, 24))}</td>
                 <td>{CalcAge(index, 24)}</td>
                 <td>{CalcReck(index, 18)}</td>
-                {/* <td>{CalcYeniLoar(CalcAge(index, 18))}</td> */}
+                <td>{CalcYeniLoar(CalcAge(index, 18))}</td>
                 <td>{CalcAge(index, 18)}</td>
                 <td>{CalcReck(index, 20)}</td>
-                {/* <td>{CalcYeniLoar(CalcMEAge(index, 20))}</td> */}
-                <td>{CalcMEAge(index)}</td>
-                {/* <td>{CalcYeniLoar(CalcMEFourthAgeOne(index, 20))}</td> */}
+                <td>{CalcYeniLoar(CalcMEAge(index, 20))}</td>
+                <td>{CalcMEAge(index, 20)}</td>
+                <td>{CalcYeniLoar(CalcMEFourthAgeOne(index))}</td>
                 <td>{CalcMEFourthAgeOne(index)}</td>
-                {/* <td>{CalcYeniLoar(CalcMEFourthAgeTwo(index, 20))}</td> */}
+                <td>{CalcYeniLoar(CalcMEFourthAgeTwo(index, 20))}</td>
                 <td>{CalcMEFourthAgeTwo(index)}</td>
                 <td>{CalcReck(index, 18)}</td>
+                <td>{CalcYeniLoar(CalcAgeSixtyFive(index))}</td>
                 <td>{CalcAgeSixtyFive(index)}</td>
             </tr>
         )
@@ -107,29 +108,72 @@ export default class ElvishAges extends Component {
                 <table class="table align-top table-bordered table-dark table-sm text-center w-auto">
                     <thead>
                         <tr>
-                            <th colSpan={9}>Elvish Aging (1959)</th>
-                            <th colSpan={2}>Elvish Aging (1965)</th>
+                            <th colSpan={17}>Elvish Aging</th>
                         </tr>
                         <tr>
-                            <th rowSpan={2}>Age</th>
-                            <th colSpan={4}>Aman</th>
-                            <th colSpan={4}>Middle-Earth (Ages)</th>
-                            <th rowSpan={2}>Age</th>
-                            <th rowSpan={2}>Elvish<br />Reckoning</th>
+                            <th colSpan={14}>1959</th>
+                            <th colSpan={4}>1965</th>
                         </tr>
                         <tr>
-                            <th>Elvish<br />Reckoning</th>
-                            <th>Male</th>
-                            <th>Elvish<br />Reckoning</th>
-                            <th>Female</th>
-                            <th>Elvish<br />Reckoning</th>
-                            <th>1st-3rd</th>
-                            <th>4th Age<br />(Early)</th>
-                            <th>4th Age<br />(Later)</th>
+                            <th rowSpan={3}>Age</th>
+                            <th colSpan={6}>Aman, The Blessed Realm</th>
+                            <th colSpan={7}>Middle-Earth (Ages)</th>
+                            <th colSpan={2} rowSpan={3}>Age</th>
+                            <th rowSpan={3}>Years</th>
+                        </tr>
+                        <tr>
+                            <th colSpan={3}>Male</th>
+                            <th colSpan={3}>Female</th>
+                            <th colSpan={3}>1st-3rd</th>
+                            <th colSpan={4}>4th</th>
+                        </tr>
+                        <tr>
+                            <th colSpan={2}>Age, Reckoning</th>
+                            <th>Years</th>
+                            <th colSpan={2}>Age, Reckoning</th>
+                            <th>Years</th>
+                            <th colSpan={2}>Age, Reckoning</th>
+                            <th>Years</th>
+                            <th colSpan={2}>Early</th>
+                            <th colSpan={2}>Later</th>
                         </tr>
                     </thead>
                     <tbody>
                         <Rows />
+                    </tbody>
+                </table>
+                <table class="table align-top table-bordered table-dark table-sm text-center w-auto">
+                    <thead>
+                        <tr>
+                            <th colSpan={3}>Legend</th>
+                        </tr>
+                        <tr>
+                            <th>Abbr.</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>GY</td>
+                            <td>Growth Years</td>
+                            <td>The period of physical growth, (<i>Olmen</i>, pl. <i>Olmendi</i>).</td>
+                        </tr>
+                        <tr>
+                            <td>LY</td>
+                            <td>Life Years</td>
+                            <td>The period of adulthood, (<i>Coimen</i>, pl. <i>Coimendi</i>).</td>
+                        </tr>
+                        <tr>
+                            <td>y</td>
+                            <td><i>Yén</i> (pl. <i>Yéni</i>)</td>
+                            <td>An Elvish long year of 144 solar years.</td>
+                        </tr>
+                        <tr>
+                            <td>l</td>
+                            <td><i>Löa</i> (pl. <i>Löar</i>)</td>
+                            <td>An Elvish solar year.</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
